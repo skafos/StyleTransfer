90ab88889028f6702c24b6621c0e9c1f374e22f3
Tyler <tch6zf@virginia.edu>
2019-03-27 11:15:11 -0400

    standardize publishable key bits

--- StyleTransfer/AppDelegate.swift
+++ StyleTransfer/AppDelegate.swift
@@ -18,9 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     self.window = app.window
 
-    // Publishable key is required
-    Skafos.initialize("<YOUR SKAFOS PUBLISHABLE KEY>")
-
+    // Skafos publishable key is required
+    // You can find it under "app settings" in your project on the Skafos dashboard
+    Skafos.initialize("<PUBLISHABLE KEY>")
+    
     return app.dispatch(launchOptions)
   }
 
